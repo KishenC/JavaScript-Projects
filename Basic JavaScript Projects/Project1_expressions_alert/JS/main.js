0b16f1947c820b7a67d84bd3024e5a6e3b54f3df
Kishen Chohan <68403624+KishenC@users.noreply.github.com>
2025-05-18 14:33:19 +0100

    inital commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+window.alert("Hello, World!");     //window.alert method
+document.write("Hello, World again!");  //document.write method
+var Sent1 = " This is the beginning of the string"; //creating 2 varibles
+var Sent2 = " and this is the end of the string ";
+document.write(Sent1 + Sent2);      //display the 2 variables together using document.write method
+document.write(90*90);      //using an expression method (*,+,-,/)
