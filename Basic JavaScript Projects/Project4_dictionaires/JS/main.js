92d53dfdf0d0bfebcf5068c85779ad1ed5f54136
Kishen Chohan <68403624+KishenC@users.noreply.github.com>
2025-05-26 22:04:36 +0100

    inital commit

--- Basic JavaScript Projects/Project4_dictionaires/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaires/JS/main.js
@@ -1,12 +1,12 @@
 function Dictionary() {
     var VideoGames = {
-        "Game": "Grand Theft Auto IV",
-        "Genre": "Action-adventure",
-        "Platform": "PS3",
-        "Release": "April 29, 2008",
-        "Developer": "Rockstar North",
-        "Age Rating": "18+",
+        Game: "Grand Theft Auto IV",
+        Genre: "Action-adventure",
+        Platform: "PS3",
+        Release: "April 29, 2008",
+        Developer: "Rockstar North",
+        Age_Rating: "18+",
     };
     delete VideoGames.Release; //This removes the Release KVP from the Dictionary before it is displayed
-    document.getElementById("Dictionary").innerHTML = VideoGames.Developer;
+    document.getElementById("Dictionary").innerHTML = VideoGames.Release;
 }
